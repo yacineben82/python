2c798921567ca2e1cd887d017dce2a141065f1bf
yacben <139992005+yacineben82@users.noreply.github.com>
2024-05-14 15:05:46 -0400

    division.py
    quotient et modulo

---
+++ division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+print("quotient et modulo")
+
+nb1=input('Donnez le dividende : ')
+nb2=input('Donnez le diviseur : ')
+
+print('le quotient est {0} et le modulo {1}'.format(int(nb1)//int(nb2), int(nb1)%int(nb2)))
