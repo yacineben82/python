06064e2bb665e6571bdc42e55fbda3d6063122dd
yacben <139992005+yacineben82@users.noreply.github.com>
2024-05-14 15:03:08 -0400

    perimetre_surface_cercle.py
    périmètre et surface d un cercle

---
+++ perimetre_surface_cercle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+print("perimetre et surface d un cercle")
+
+c=input("Donnez le rayon du cercle : ")
+
+print("la surface du cercle est {0} et le primetre est {1}".format(round(2*3.14*(int(c)),2), round(3.14*int(c)*int(c),2)))
